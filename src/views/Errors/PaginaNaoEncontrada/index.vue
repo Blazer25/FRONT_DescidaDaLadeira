73e370e90ca0b5182c5033cb65f9fcf65c32f9cf
Eugenio <joaovitor.eugenio15@gmail.com>
2023-12-09 00:30:15 -0300

    feat: adicionada página de erros/página não encontrada.

---
+++ src/views/Errors/PaginaNaoEncontrada/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <MenuLateralLogo />
+
+    <div class="informacoes">
+      <p class="titulo">Página não encontrada</p>
+      <img
+        src="src/assets/images/gifs/404.gif"
+        alt="pagina não encontrada"
+        class="gif"
+      />
+      <p @click="redirecionar('home')" class="cursor-pointer texto">Clique aqui para voltar a <span class="azul">página inicial</span></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaginaNaoEncontrada",
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    redirecionar(pagina) {
+      const rotas = {
+        home: "/home",
+      };
+
+      const rotaDestino = rotas[pagina];
+
+      if (rotaDestino) {
+        this.$router.push(rotaDestino);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./styles.scss" />
