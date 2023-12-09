2ab15faf64da3d312d038081fc5ba732035a20b9
Eugenio <joaovitor.eugenio15@gmail.com>
2023-12-09 00:26:05 -0300

    feat: desenvolvido componente de aviso padrão.

---
+++ src/common/components/AvisoPadrao/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="exibir" class="aviso-erro">
+    {{ mensagem }}
+    <button type="button" class="fechar" @click="fecharAviso">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  props: {
+    mensagem: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    exibir: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  methods: {
+    fecharAviso() {
+      this.$emit("fechar-aviso")
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./styles.scss" />
