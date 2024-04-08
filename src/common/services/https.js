bec5ac43c2134f528dd85de0d37f83605c7870ac
Eugenio <joaovitor.eugenio15@gmail.com>
2024-04-08 03:45:50 -0300

    fix: correção do serviço de https para verificar se o token já existe em localstorage.

--- src/common/services/https.js
+++ src/common/services/https.js
@@ -1,11 +1,31 @@
 import axios from "axios";
 
+const atualizarTokenAutorizacaoHeader = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    instanciaAxios.defaults.headers.common["Authorization"] = token;
+  } else {
+    delete instanciaAxios.defaults.headers.common["Authorization"];
+  }
+};
+
 const instanciaAxios = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
-    Authorization: localStorage.getItem('token') || null
-  }
+  },
 });
+
+instanciaAxios.interceptors.request.use(
+  (config) => {
+    atualizarTokenAutorizacaoHeader();
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+atualizarTokenAutorizacaoHeader();
 
 export default instanciaAxios;
